f77bd47b22a4872ad2f10b35997149c52834a61e
wuyanqiang <461804160@qq.com>
2019-11-13 23:25:52 +0800

    Initial Commit

---
+++ TestOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestOpenCV
+//
+//  Created by johnwu on 2019/11/13.
+//  Copyright © 2019年 johnwu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
